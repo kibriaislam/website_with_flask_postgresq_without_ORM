b9d6928aec3370130fecd7c4eea5e4e774b72935
Kibria Islam <kibriaislam82@gmail.com>
2022-06-09 06:19:25 +0600

    database connection are updated

--- website/blog/blog_bp.py
+++ website/blog/blog_bp.py
@@ -1,3 +1,29 @@
 from flask import Blueprint, render_template
+from db_connection import cursor
 
+
+main = Blueprint('main', __name__,
+    template_folder='templates',
+    static_folder='static',)
+
+@main.route('/')
+def blog_list():
+
+    # query = '''
+    # SELECT content,user_name,posts.create_at 
+    # FROM posts
+    # JOIN users ON  users.id = posts.user_id
+    # '''
+
+    query = '''SELECT * FROM posts'''
+
+    cursor.execute(query)
+    data =cursor.fetchall()
+
+    print("kibria: ", data)
+
+
+
+
+    return render_template('home.html', data = data)
 
--- website/db_connection.py
+++ website/db_connection.py
@@ -1,8 +1,11 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+
 
 #establishing the connection
-conn = psycopg2.connect(
-   database="blogsite_db", user='postgres', password='alterbridge', host='localhost', port= '5432'
-)
+conn = psycopg2.connect(database=os.environ['db_name'], user='postgres', password=os.environ['db_password'], host='localhost', port= '5432')
 #Creating a cursor object using the cursor() method
 cursor = conn.cursor()
