98f268f83cc14461d7555656297fa18669ef2b43
Kibria Islam <kibriaislam82@gmail.com>
2022-06-10 19:55:20 +0600

    fetch data are organised

--- website/blog/blog_bp.py
+++ website/blog/blog_bp.py
@@ -9,21 +9,26 @@ main = Blueprint('main', __name__,
 @main.route('/')
 def blog_list():
 
-    # query = '''
-    # SELECT content,user_name,posts.create_at 
-    # FROM posts
-    # JOIN users ON  users.id = posts.user_id
-    # '''
+    query = '''
+    SELECT content,user_name,posts.create_at 
+    FROM posts
+    JOIN users ON  users.id = posts.user_id
+    '''
 
-    query = '''SELECT * FROM posts'''
+    # query = '''SELECT * FROM posts'''
 
     cursor.execute(query)
     data =cursor.fetchall()
+    result = []
+
+    for i in data:
+        result.append({"content":i[0],"user_name":i[1],'created_at':i[2]})
+
 
     print("kibria: ", data)
 
 
 
 
-    return render_template('home.html', data = data)
+    return render_template('home.html', data = result)
 
