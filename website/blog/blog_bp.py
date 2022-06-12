40831774a246667d7df34c10f0f1ed34577ad93d
Kibria Islam <kibriaislam82@gmail.com>
2022-06-13 01:18:33 +0600

    new funtionality added in blog section

--- website/blog/blog_bp.py
+++ website/blog/blog_bp.py
@@ -1,8 +1,9 @@
+from sqlite3 import Cursor
 from flask import Blueprint, render_template
 from db_connection import db_conn
 
 
-blog_list_bp = Blueprint('main', __name__,
+blog_list_bp = Blueprint('blog_list_bp', __name__,
     template_folder='templates',
     static_folder='static',)
 
@@ -12,7 +13,7 @@ def blog_list():
     cursor = conn.cursor()
 
     query = '''
-    SELECT title,content,user_name,posts.create_at 
+    SELECT posts.id,title,content,user_name,posts.create_at 
     FROM posts
     JOIN users ON  users.id = posts.user_id
     '''
@@ -24,9 +25,58 @@ def blog_list():
     result = []
 
     for i in data:
-        result.append({"title":i[0],"content":i[1],"user_name":i[2],'created_at':i[3]})
+        result.append({"id":i[0],"title":i[1],"content":i[2],"user_name":i[3],'created_at':i[4]})
 
     conn.close()
 
     return render_template('home.html', data = result)
+
+
+@blog_list_bp.route('/<int:id>')
+def blog_details(id):
+
+    conn = db_conn()
+    cursor = conn.cursor()
+
+    query = '''
+    SELECT posts.id,title,content,user_name,posts.create_at
+    FROM posts
+    JOIN users ON users.id = posts.user_id
+    WHERE posts.id = %s
+    ''' % (id)
+
+    cursor.execute(query)
+    data = cursor.fetchall()
+    result = []
+
+    for i in data:
+        result.append({"id":i[0],"title":i[1],"content":i[2],"user_name":i[3],'created_at':i[4]})
+    
+    conn.close()
+
+    return render_template('home.html',data = result)
+
+@blog_list_bp.route("/latest")
+def latest_posts():
+
+    conn = db_conn()
+
+    cursor = conn.cursor()
+
+    query = '''
+    SELECT posts.id,title,content,user_name,posts.create_at
+    FROM posts
+    JOIN users ON users.id = posts.user_id
+    ORDER BY posts.id DESC
+    '''
+    cursor.execute(query)
+    data = cursor.fetchall()
+    result = []
+
+    for i in data:
+        result.append({"id":i[0],"title":i[1],"content":i[2],"user_name":i[3],'created_at':i[4]})
+    
+    conn.close()
+
+    return render_template('home.html',data = result)
 
